2172ac5c38e3bb97331ae642fbb8ee8da86201c7
arafat2020 <arafatmannan2019@gmail.com>
2025-07-27 01:00:43 +0600

    minor adjustment

--- src/main/java/com/interview/interview/auth/AuthController.java
+++ src/main/java/com/interview/interview/auth/AuthController.java
@@ -6,32 +6,66 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.Data;
 
 @RestController
 @Tag(name = "Authentication", description = "Endpoints for user authentication")
 public class AuthController {
-    @PostMapping("/login")
+
+    @PostMapping("/register")
     @Operation(summary = "Authentication Endpoint", description = "Endpoint for user login")
     public AuthResponse login(@RequestBody AuthRequest request) {
         System.out.println(request.password + request.username);
         return new AuthResponse("fake-token");
     }
 
-     public static class AuthRequest {
+    @PostMapping("path")
+    @Operation(summary = "Registration Endpoint", description = "Endpoint for user registration")
+    public String register(@RequestBody RegisterRequest entity) {
+
+        return entity.toString();
+    }
+
+    public static class AuthRequest {
         private String username;
         private String password;
-        public AuthRequest() {}
+
+        public AuthRequest() {
+        }
+
         public AuthRequest(String username, String password) {
             this.username = username;
             this.password = password;
         }
-        public String getUsername() { return username; }
-        public String getPassword() { return password; }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
     }
 
     public static class AuthResponse {
         private String token;
-        public AuthResponse(String token) { this.token = token; }
-        public String getToken() { return token; }
+
+        public AuthResponse(String token) {
+            this.token = token;
+        }
+
+        public String getToken() {
+            return token;
+        }
+    }
+
+    @Data
+    public class RegisterRequest {
+        private String name;
+        private String phone;
+        private String email;
+        private String password;
+        private String confirmPassword;
+        private Boolean aggriedToTerms;
     }
 }
